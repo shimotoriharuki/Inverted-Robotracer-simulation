1beb9ea5360c78f69bca2b52573f45382eccde74
Haruki <underbird.h@gmail.com>
2024-04-14 08:35:31 +0900

    よさげ

---
+++ parameters.m
@@ -0,0 +1,14 @@
+ground.dimensions = [1, 1, 0.01];
+
+chassis.dimentions = [0.010, 0.05, 0.1];
+tire.geometry.radius = 0.02;
+tire.geometry.length = 0.01;
+
+
+spur_gear.geometry.radius = 0.01;
+spur_gear.geometry.length = 0.005;
+
+pinion_gear.geometry.radius = 0.005;
+pinion_gear.geometry.length = 0.005;
+
+center_distance = 0.02;
